6c08c5619cda2d10c467300847610b5d6f2c79fe
mariuchasilva <78319598+mariuchasilva@users.noreply.github.com>
2025-05-23 11:47:06 -0300

    primeiras variaveis

--- olamundo.py
+++ olamundo.py
@@ -1,1 +1,12 @@
-print("Ola mundo")
+print("Ola mund")
+print("Mariucha")
+#Variavel string
+nome = 'Mariucha'
+#Variavel int
+ano_nascimento = 1984
+#Variavel booleana
+foi_pro_show_da_gaga = False
+maior_de_idade = 2025 - ano_nascimento >= 18
+print('Mariucha e maior de idade?', maior_de_idade)
+#Variaveis podem admitir valores nulos
+nota_sobre_o_prefeito = ''
