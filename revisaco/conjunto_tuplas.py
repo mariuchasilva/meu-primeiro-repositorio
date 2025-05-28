969c75eaaa0d05c27604423b0988f2e90f20e9ab
mariuchasilva <78319598+mariuchasilva@users.noreply.github.com>
2025-05-28 10:37:27 -0300

    Exercicio tuplas

---
+++ revisaco/conjunto_tuplas.py
@@ -0,0 +1,54 @@
+'''Questões de Python com Conjuntos e Tuplas'''
+
+'''Nível Estagiário'''
+
+'''1 - Crie uma tupla chamada `cores` contendo os elementos `'vermelho'`, `'azul'`, e `'verde'`.
+Em seguida, imprima o segundo elemento da tupla.'''
+print('tupla cores')
+#escreva o código aqui
+
+'''2 - Dada a tupla `numeros = (10, 20, 30, 40, 50)`, acesse e imprima o terceiro elemento.'''
+#escreva o código aqui
+
+'''3 - Crie um conjunto chamado `frutas` com os elementos `'maçã'`, `'banana'`, `'laranja'`, e `'uva'`.
+Em seguida, adicione o elemento `'abacaxi'` ao conjunto.'''
+#escreva o código aqui
+
+'''4 - Dado os conjuntos `A = {1, 2, 3, 4}` e `B = {3, 4, 5, 6}`, 
+imprima a **união** e a **interseção** entre os dois conjuntos.'''
+#escreva o código aqui
+
+'''5 - Escreva uma função `contar_unicos(lista)` que receba uma lista e retorne a quantidade de elementos únicos
+utilizando conjuntos.
+Exemplo: contar_unicos(['deyverson', 'hulk', 'menin', 'menin', bernard]) deve retornar 4'''
+def contar_unicos(lista):
+  print('contar_unicos(lista)')
+  #remova o print acima e escreva o código aqui
+#não esqueça de chamar a função (com argumentos, se necessário) aqui abaixo!
+
+'''6. Convertendo Lista para Tupla:
+Escreva uma função `converter_para_tupla(lista)` que receba uma lista e retorne uma tupla com
+os mesmos elementos.
+Exemplo:
+lista_comentada = [10, 20, 30, 40]
+print(converter_para_tupla(lista_comentada)) deve retornar (10, 20, 30, 40)'''
+def converter_para_tupla(lista):
+  print('converter_para_tupla(lista)')
+#remova o print acima e escreva o código aqui
+#não esqueça de chamar a função (com argumentos, se necessário) aqui abaixo!
+
+'''10. Desempacotando Tuplas:
+Dada a tupla `pessoa = ('Ana', 25, 'Engenheira')`, desempacote os elementos da tupla em variáveis individuais e
+imprima cada uma.'''
+#escreva o código aqui
+
+#boa sorte
+'''Escreva uma função `remover_duplicados(lista)` que receba uma lista e
+retorne uma nova lista com os elementos únicos, mantendo a ordem original dos elementos.
+Exemplo: lista_exemplo = [1, 2, 2, 3, 4, 4, 5]
+remover_duplicados(lista_exemplo) deve retornar [1, 2, 3, 4, 5]
+'''
+def remover_duplicados(lista):
+    print('remover_duplicados(lista)')
+#remova o print acima e escreva o código aqui
+#não esqueça de chamar a função (com argumentos, se necessário) aqui abaixo!
